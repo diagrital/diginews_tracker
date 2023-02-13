bcb9b9d51549a6b238b236620808e60919c62308
diagrital <124864324+diagrital@users.noreply.github.com>
2023-02-13 13:58:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,15 @@ import pandas as pd
 #st.title("Hello world!")
 
 uploaded_file = st.file_uploader("Choose a file")
+print(uploaded_file)
 
-pulse_value = st.sidebar.selectbox("Select pulse", [0,1,2,3,4,5], index=0)
-pulse_value_2 = st.sidebar.selectbox("Select pulse 2",[0,1,2,3,4,5],index = 0)
 
 
 if uploaded_file is not None:
   df = pd.read_excel(uploaded_file)
   #cols = st.sidebar.selectbox("Number of columns you want in your data", list(range(len(df.columns))), index=0)
+  pulse_value = st.sidebar.selectbox("Select pulse", [0,1,2,3,4,5], index=0)
+  pulse_value_2 = st.sidebar.selectbox("Select pulse 2",[0,1,2,3,4,5],index = 0)
   df = df[(df['Pulse'] == pulse_value) & (df['Pulse'] == pulse_value_2)]
   
   df = df[["Name", "Phone Number", "Speaker"]]
@@ -24,7 +25,7 @@ if uploaded_file is not None:
   #df = pd.read_csv("dir/file.csv")
   @st.experimental_memo
   def convert_df(df):
-      return df.to_excel(index=False)
+      return df.to_excel('index=False)
   excel = convert_df(df)
   st.download_button("Press to Download",excel,"file.csv","text/csv",
   key='download-file')
